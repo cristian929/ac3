a697fb6f1ee81c2185d6ea5739804a737ad51e37
cristian929 <cristianlucas180@gmail.com>
2020-10-28 16:42:20 -0300

    ac3

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python msg.py
+web: python t9.py
 
---
+++ t9.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def numeros_primos():
+
+    limite = 100
+    c = 1
+    inicio = 1
+    numero = 3
+
+    primos = "2, "
+
+    while inicio < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            inicio += 1
+            if(inicio % 10 == 0):
+                primos = primos + "<br>"
+        numero += 1
+
+    return primos
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
